5d04a9272cd78c8e04cb1fda5be704d62f0f9df2
Anthony Gorski <anthony.developpeurweb@gmail.com>
2022-09-28 15:30:48 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Quiz JavaScript
+
+## But de l'exercice 🎯
+
+Le but est de manipuler le DOM et de créer un quiz en JavaScript.
+
+## Étape 1️
+
+Enregistrer les bonnes reponses dans un tableau
+
+## Étape 2️
+
+Créer 2 tableaux vide
+
+## Étape 3️
+
+Faire un querySelector du formulaire
+
+## Étape 4️
+
+Ecouter l'evenement "Submit" (n'oubliez pas de faire attention au refresh de la page avec 'event')
+
+---
+
+## Bonus 🚀
+
+Si vous avez des idées et des implémentation supplémentaire, n'hésitez pas à vous amuser... C'est comme ça qu'on apprend ! ❤️
+
+---
+
+🏠 [Main Menu](README.md)
+
+# Author
+
+👤 [Anthony Gorski](https://twitter.com/Gorski_anthony)
+
---
+++ app.js
@@ -0,0 +1,24 @@
+/******************************************/
+//              Consigne                  //
+/******************************************/
+
+/**
+ * Etape
+ *
+ * 1. Enregistrer les bonnes reponses dans un tableau
+ * 2. Créer 2 tableaux vide pour avoir les réponses de l'utilisateur & pour verifier si c'est les bonnes
+ * 3. Faire un querySelector du formulaire
+ * 4. Ecouter l'evenement "Submit" du *formulaire* (n'oubliez pas de faire attention au refresh de la page avec 'event')
+ * doc : https://developer.mozilla.org/fr/docs/Web/API/HTMLFormElement/submit_event_
+ *
+ * Suite
+ *
+ * 1. Faire une boucle for sur la longeur du tableau des réponses juste.
+ * 2. *Push* les informations récuperer dans le tableau RESPONSE_USER.
+ * 3. Fonction ' checkIsTrue' qui prend en paramètre le tableau de RESPONSE_USER
+ *
+ * La fonction 'checkIsTrue' va comparer dans une boucle for si les reponses sont juste ou fausse.
+ * Si oui, push 'true' sinon push 'false
+ */
+
+/********************** Exercice **********************/
